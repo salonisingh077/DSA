2c67d8ea648f2bbf0d2432b829e87f8a67c6cda6
salonisingh077 <salonisingh0705@gmail.com>
2025-11-03 00:02:01 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (66.65%) - LeetSync

---
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+     int n=word.size();
+     int idx=-1;
+     for(int i=0;i<n;i++){
+        if(word[i]== ch){idx=i;break;}
+     } 
+     if(idx<=0) return word;
+     reverse(word.begin(),word.begin()+idx+1);
+     return word;
+    }
+};
