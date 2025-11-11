0792cd1125f30069bc15fcb5b493104b991c0c03
salonisingh077 <salonisingh0705@gmail.com>
2025-11-11 22:05:22 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (57.68%) - LeetSync

---
+++ 415-add-strings/add-strings.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        int i=num1.size()-1;
+        int j=num2.size()-1;
+        int carry=0;
+        string res;
+        res.reserve(max(num1.size(),num2.size())+1);
+        while(i>=0||j>=0||carry){
+            int sum=carry;
+            if(i>=0)sum+=num1[i--]-'0';
+            if(j>=0)sum+=num2[j--]-'0';
+            res+=(sum%10)+'0';
+            carry=sum/10;
+
+        }
+        reverse(res.begin(),res.end());
+        return res;
+    }
+};
