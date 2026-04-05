6ea6f810c39d9bfb52e1161ad25ba3b5f967c98c
salonisingh077 <salonisingh0705@gmail.com>
2026-04-05 21:34:28 +0530

    Time: 2 ms (10.04%) | Memory: 30.2 MB (25.73%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -4,17 +4,16 @@ public:
      int n=nums.size();
      k=k%n;
      vector<int> temp;
-     for(int i= n-k;i<n;i++){
+     for(int i=n-k;i<n;i++){
         temp.push_back(nums[i]);
-     }
+    }
      for(int i=n-k-1;i>=0;i--){
         nums[i+k]=nums[i];
-
-     }
-     for(int i =0;i<k;i++){
+    }
+    for(int i=0;i<k;i++){
         nums[i]=temp[i];
-     }
-     
+    }
+   
     }
     
 };
